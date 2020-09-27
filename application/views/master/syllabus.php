213e299d2b45cd1145daee79b9149aa44b01e71a
baskar srinivasan <baskar@alphasoftz.in>
2020-09-27 13:22:19 +0530

    tnpsc back end

--- application/views/master/syllabus.php
+++ application/views/master/syllabus.php
@@ -1,14 +1,13 @@
 <?php foreach($exam_syllabus as $row){}?>
-<main class="bg_color">
-
-        <div class="hero_single version_2">
+    
+    <main class="bg_color">
+         <div class="hero_single version_2">
             <div class="opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xl-9 col-lg-10">
-                            <h2 style="color:#fff;padding-top: 20px;">BUILD YOUR DREAM JOB HERE</h2>
-                            <br/>
-                            <a href="<?php echo base_url()?>register" class="btn_1 medium">START NOW</a>
+                        <div class="col-xl-12 col-lg-10">
+                            <h2 class="upper" style="color:#fff;padding-top:0px;text-align: center;">Exam Syllabus</h2>
+                           
                             
                         </div>
                     </div>
@@ -16,52 +15,43 @@
                 </div>
             </div>
         </div>
-        
-        <div class="container margin_30_40">
-                
+        <div class="container margin_detail">
             <div class="row">
+                <div class="col-xl-8 col-lg-7">
+                    <div class="box_general">
+                        
+                        <div class="main_info_wrapper">
+                            
+                            <div class="main_info clearfix">
+                                <div class="user_desc">
+                                    <h3 class="center" style="text-align:center;">Exam Syllabus</h3>
+                                    
+                                </div>
+                                
+                            </div>
+                            
+
+                            <div class="services_list clearfix">
+                                                    <ul>
+                                                        <?php foreach($exam_syllabus as $syllabus){?>
+                        <li class="upper"><a href="<?php echo base_url()?>exam/<?php echo $syllabus->id;?>"><i class="icon_compass_alt"></i><span style="padding-left: 5px"/><?php echo $syllabus->exam_syllabus_name;?></a></li>
+                    <?php } ?>
+                                                       
+                                                    </ul>
+                                                </div>
+                        </div>
+                        <!-- /main_info -->
+                    </div>
+                    <!-- /box_general -->
+                                    </div>
+                <!-- /col -->
                 <?php include("left_menu1.php");?>
 
-                <div class="col-lg-8">
-                    <div class="row" style="border:1px solid #000c34;">
-                <div class="col-12" style="background: #000c34">
-                    <div class="" style="padding-top: 10px">
-                    
-                    <h4 title="Tnpsc Career Popular Exams" style="color:#fff;text-transform: uppercase;">syllabus</h4>
-                    
-                </div>
-                </div>
-                <div class="col-md-12">
-                    
-                       
-                               <div class="container">
-            
-  <table class="table">
-    
-    <tbody>
-        <?php foreach($exam_syllabus as $syllabus){?>
-      <tr>
-        <td ><a style="color:blue;font-size: 18px" href="<?php echo base_url()?>exam/<?php echo $syllabus->id;?>"><?php echo $syllabus->exam_syllabus_name;?></a></td>
-        
-       
-       
-      </tr>
-  <?php } ?>
-      
-    </tbody>
-  </table>
-  
-</div>
-                   
-                </div>
-                
             </div>
-                </div>
-               
-                <!-- /col -->
-            </div>      
+            <!-- /row -->
         </div>
         <!-- /container -->
-        
     </main>
+    <!-- /main -->
+
     
