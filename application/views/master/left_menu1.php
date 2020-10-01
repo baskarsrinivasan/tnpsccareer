7ca13041775f2d26f596b10dc9ee6606df530cc9
baskar srinivasan <baskar@alphasoftz.in>
2020-10-01 15:53:34 +0530

    cobonat front end

--- application/views/master/left_menu1.php
+++ application/views/master/left_menu1.php
@@ -13,7 +13,14 @@
                                                         <?php foreach($exams as $row){?>
                                                         <li><a title="<?php echo $row->exams_name;?> Syllabus" href="<?php echo base_url()?>exam/<?php echo $row->id;?>" style="color:#000"><?php echo $row->exams_name;?></a></li>
                                                         <?php } ?>
-                                                       
+                                                        <li>
+                                                            
+                                                            <a target="_blank" href="https://www.amazon.in/b?_encoding=UTF8&tag=bafa151515-21&linkCode=ur2&linkId=99d2e2d89925b297ad1f2c692d52a5e0&camp=3638&creative=24630&node=4149751031">Study Material</a><img src="//ir-in.amazon-adsystem.com/e/ir?t=bafa151515-21&l=ur2&o=31" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+                                                        </li>
+                                                       <li>
+                                                           <a target="_blank" href="https://www.amazon.in/b?_encoding=UTF8&tag=bafa151515-21&linkCode=ur2&linkId=4d78e6122f42efa7f9c80f5cd27247e1&camp=3638&creative=24630&node=15417300031">Textbooks &amp; study guides</a><img src="//ir-in.amazon-adsystem.com/e/ir?t=bafa151515-21&l=ur2&o=31" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+                                                       </li>
+
                                                     </ul>
                                                     <iframe style="width:100%;height:240px;" marginwidth="400px" marginheight="0" scrolling="no" frameborder="0" src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bafa151515-21&marketplace=amazon&region=IN&placement=9352832167&asins=9352832167&linkId=0ea6a5d9c97607d306390ad059485b01&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff">
     </iframe>
