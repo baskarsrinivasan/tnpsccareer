1d400a88c3886465ff755fdd8aa09c5e52ba98eb
baskar srinivasan <baskar@alphasoftz.in>
2020-10-14 10:55:17 +0530

    tnpsccareer

--- application/views/master/question_paper_details.php
+++ application/views/master/question_paper_details.php
@@ -37,7 +37,9 @@
                                 <?php echo $row->exam_notification_des;?>
                             </div>
                             <!-- /content_more -->
-                            <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5f4c9ea0dc8707001906e0e1&product=sop' async='async'></script>
+                            <!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d451b0cd47cc57b"></script>
+
                         </div>
                         <!-- /main_info -->
                     </div>
